21100ed90f82378b081cde34126a0325c3f9362c
nipunattri1 <nipun.acads@gmail.com>
2026-02-27 21:37:43 +0530

    fix/apply-db-migrations

---
+++ prisma/migrations/20260227160701_v1/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `attending` on the `Response` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "Response_rollNo_key";
+
+-- AlterTable
+ALTER TABLE "Response" DROP COLUMN "attending";
