79484af42457d44d081d5aec7cc00d986f0287b7
Shanjiv A <shanjiv177@gmail.com>
2026-02-27 18:04:08 +0530

    remove roll number in form

--- app/letter/actions/rsvp.tsx
+++ app/letter/actions/rsvp.tsx
@@ -1,15 +1,12 @@
-'use server';
-import { prisma } from '@/lib/prisma'; // Using the singleton we set up earlier
+"use server";
+import { prisma } from "@/lib/prisma"; // Using the singleton we set up earlier
 
-export async function createRegistration(data: {
-  name: string;
-  rollNo: string;
-}) {
+export async function createRegistration(data: { name: string }) {
   // Now you can use Prisma to save it!
   const user = await prisma.response.create({
     data: {
       name: data.name,
-      rollNo: data.rollNo,
+      rollNo: "NA", // Lite on changing schema :)
     },
   });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { createRegistration } from "./letter/actions/rsvp";
 
 export default function Home() {
-
   const router = useRouter();
 
-  const [name, setName] = useState("")
-  const [rollNo, setRollNo] = useState("")
-  const [loading, setLoading] = useState(false)
+  const [name, setName] = useState("");
+  // const [rollNo, setRollNo] = useState("")
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(e: React.SubmitEvent) {
     e.preventDefault(); // Stop the page reload
     setLoading(true);
     try {
-      await createRegistration({ name, rollNo });
+      await createRegistration({ name });
       const encodedName = encodeURIComponent(name);
       router.push(`/letter?name=${encodedName}`);
     } catch (error) {
@@ -28,17 +27,20 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white font-serif p-4 md:p-16 text-black">
       <div className="max-w-4xl mx-auto border-t-[3px] border-black pt-8">
-
         <div className="flex flex-row justify-between items-start gap-4 mb-10">
           <div className="space-y-1 text-sm min-w-0">
             <div className="grid grid-cols-[100px_1fr] gap-x-2">
               <span className="font-bold">From:</span>
-              <span className="break-all md:break-normal">Team IRIS &lt;team@iris.nitk.ac.in&gt;</span>
+              <span className="break-all md:break-normal">
+                Team IRIS &lt;team@iris.nitk.ac.in&gt;
+              </span>
             </div>
 
             <div className="grid grid-cols-[100px_1fr] gap-x-2">
               <span className="font-bold">To:</span>
-              <span className="break-all md:break-normal">IRIS 26 &lt;iris26@iris.nitk.ac.in&gt;</span>
+              <span className="break-all md:break-normal">
+                IRIS 26 &lt;iris26@iris.nitk.ac.in&gt;
+              </span>
             </div>
 
             <div className="grid grid-cols-[100px_1fr] gap-x-2">
@@ -71,19 +73,56 @@ export default function Home() {
         <div className="mb-12 text-[15px] leading-snug">
           <div className="mb-12 text-[15px] leading-snug">
             <p className="mb-9 font-normal leading-relaxed">
-              Do you think the party is unfit for you. Dw no one is over <span className="bg-black highlight:text-white px-1 font-bold">_____</span>, and all are very <span className="bg-black highlight:text-white px-1 font-bold">____</span>.  I have planted the seed with our invite today. Hope your <span className="bg-black highlight:text-white px-1 font-bold">____</span> will be able to handle it.
-              Here is the final final. I have decided to make a change of the venue from <span className="bg-black highlight:text-white px-1 font-bold">____</span> to <Link className="underline hover:bg-black hover:text-white" href={"https://maps.app.goo.gl/Sy4vTq5QtrgUctCW9"}>The Avatar Flexi - 1st Floor</Link> to reduce the number of travel minutes. I have specially handpicked both <span className="bg-black highlight:text-white px-1 font-bold">Pizzas</span> and <span className="bg-black highlight:text-white px-1 font-bold">Drinks</span> to suit all your needs. We have pizzas, grape juice and ice creams waiting for you on 20th March 5 PM. I was unfortunately not able to get my hands on cheese and hotdog. Whoops!
+              Do you think the party is unfit for you. Dw no one is over{" "}
+              <span className="bg-black highlight:text-white px-1 font-bold">
+                _____
+              </span>
+              , and all are very{" "}
+              <span className="bg-black highlight:text-white px-1 font-bold">
+                ____
+              </span>
+              . I have planted the seed with our invite today. Hope your{" "}
+              <span className="bg-black highlight:text-white px-1 font-bold">
+                ____
+              </span>{" "}
+              will be able to handle it. Here is the final final. I have decided
+              to make a change of the venue from{" "}
+              <span className="bg-black highlight:text-white px-1 font-bold">
+                ____
+              </span>{" "}
+              to{" "}
+              <Link
+                className="underline hover:bg-black hover:text-white"
+                href={"https://maps.app.goo.gl/Sy4vTq5QtrgUctCW9"}
+              >
+                The Avatar Flexi - 1st Floor
+              </Link>{" "}
+              to reduce the number of travel minutes. I have specially
+              handpicked both{" "}
+              <span className="bg-black highlight:text-white px-1 font-bold">
+                Pizzas
+              </span>{" "}
+              and{" "}
+              <span className="bg-black highlight:text-white px-1 font-bold">
+                Drinks
+              </span>{" "}
+              to suit all your needs. We have pizzas, grape juice and ice creams
+              waiting for you on 20th March 5 PM. I was unfortunately not able
+              to get my hands on cheese and hotdog. Whoops!
               <br />
-              Honestly I'd like to speak on the phone as that's way easier.  I'll be off with snowhite then.
+              Honestly I'd like to speak on the phone as that's way easier. I'll
+              be off with snowhite then.
               <br />
-              You'll regret missing the party as it's definitely gonna be a memorable experience.
+              You'll regret missing the party as it's definitely gonna be a
+              memorable experience.
             </p>
-
           </div>
           <form onSubmit={onSubmit}>
             <div className="max-w-md space-y-6 py-4">
               <div className="flex flex-col border-l border-black/20 pl-4">
-                <label className="text-[11px] font-bold uppercase mb-1">Full Legal Name:</label>
+                <label className="text-[11px] font-bold uppercase mb-1">
+                  Full Legal Name:
+                </label>
                 <input
                   type="text"
                   className="bg-transparent border-b border-black/40 py-1 focus:outline-none focus:border-black text-[14px]"
@@ -93,7 +132,7 @@ export default function Home() {
                 />
               </div>
 
-              <div className="flex flex-col border-l border-black/20 pl-4">
+              {/* <div className="flex flex-col border-l border-black/20 pl-4">
                 <label className="text-[11px] font-bold uppercase mb-1">Roll Number:</label>
                 <input
                   type="text"
@@ -102,31 +141,50 @@ export default function Home() {
                   onChange={(e) => setRollNo(e.target.value)}
                   required
                 />
-              </div>
+              </div> */}
             </div>
             {/* <Link href={'/letter?name={subject}'} type="submit"> */}
-            <button type="submit" className={`text-[13px] font-bold underline decoration-1 underline-offset-2 ${loading ? `animate-pulse` : ``} hover:bg-black hover:text-white px-1 transition-all`}>
+            <button
+              type="submit"
+              className={`text-[13px] font-bold underline decoration-1 underline-offset-2 ${loading ? `animate-pulse` : ``} hover:bg-black hover:text-white px-1 transition-all`}
+            >
               Submit Response
             </button>
             {/* </Link> */}
           </form>
-          <div className="mt-10">
-
-          </div>
+          <div className="mt-10"></div>
         </div>
 
         <div className="mt-16 pt-4">
-          <p className="text-[13px] mb-2 font-sans">----- Original Message -----</p>
+          <p className="text-[13px] mb-2 font-sans">
+            ----- Original Message -----
+          </p>
           <div className="space-y-0 text-[13px]">
-            <p><span className="font-bold">From:</span> IRIS 26 &lt;iris26@iris.nitk.ac.in&gt;</p>
-            <p><span className="font-bold">To:</span> Team IRIS &lt;team@iris.nitk.ac.in&gt;</p>
-            <p><span className="font-bold">Sent:</span><span className="bg-black text-black px-1 font-bold">REDACTED</span></p>
-            <p><span className="font-bold">Subject:</span>seems like farewell is going to be epic</p>
+            <p>
+              <span className="font-bold">From:</span> IRIS 26
+              &lt;iris26@iris.nitk.ac.in&gt;
+            </p>
+            <p>
+              <span className="font-bold">To:</span> Team IRIS
+              &lt;team@iris.nitk.ac.in&gt;
+            </p>
+            <p>
+              <span className="font-bold">Sent:</span>
+              <span className="bg-black text-black px-1 font-bold">
+                REDACTED
+              </span>
+            </p>
+            <p>
+              <span className="font-bold">Subject:</span>seems like farewell is
+              going to be epic
+            </p>
           </div>
 
           <p className="mt-6 text-[14px] leading-relaxed">
-            We are still on the fence about the event. I need to know if the environment is going to be 'correct' for what we discussed.
-            I need a setup that is efficient and private. Let me know if you can fix the travel and if the 'entertainment' is actually what you promised.
+            We are still on the fence about the event. I need to know if the
+            environment is going to be 'correct' for what we discussed. I need a
+            setup that is efficient and private. Let me know if you can fix the
+            travel and if the 'entertainment' is actually what you promised.
             <br />
             Waiting on the final details.
           </p>
@@ -137,6 +195,6 @@ export default function Home() {
           {"*".repeat(95)}
         </div>
       </div>
-    </div >
+    </div>
   );
 }
